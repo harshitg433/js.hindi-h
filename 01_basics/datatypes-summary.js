934d1a3e2fa1e61ba60010e03be35f971c332a0b
harshitg433 <119657161+harshitg433@users.noreply.github.com>
2023-10-17 08:55:21 +0000

    datatype-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,31 @@
+// Primitive
+
+// 7 types : string, number, boolearn, null, undefined, symbol, bigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('159')
+const anotherId = Symbol('159')
+
+console.log(id == anotherId);
+
+//reference (non primitive)
+
+// Array, objects, functions 
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "harshit",
+    age: 25,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof id);
