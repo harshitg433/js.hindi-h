9c5c3f56c33d71068f4078941cbc20b523279cbc
harshitg433 <119657161+harshitg433@users.noreply.github.com>
2023-10-17 07:27:31 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "33" ==> 33
+// "33abc" ==> NaN
+// true ==> 1; false ==> 0
+
+// let isLoggedin = "sasas"
+
+// let booleanIsLoggedIn = Boolean(isLoggedin)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
