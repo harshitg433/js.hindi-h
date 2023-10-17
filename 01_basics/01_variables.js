5b7260a63a989d27792c7fa3c7ef36d40383eef1
harshitg433 <119657161+harshitg433@users.noreply.github.com>
2023-10-17 07:12:46 +0000

    datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const account_id = 1233
+let account_email = "xyz@gmail.com"
+var account_password = "526778"
+account_city = "lko"
+
+// account_id = 3
+account_email= "jhbdha@gmmail.com"
+account_password = "98/674"
+account_city = "up"
+console.log(account_id);
+console.table([account_email, account_id, account_password, account_city])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version
+
+// alert( 3 + 3) //we are using node.js ,not browser
+
+// console.log(3+3);
+
+// console.log("harshit");
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefine =>
+// symbol => unique
+
+// object
+let x = 5
+console.log(typeof undefined);
