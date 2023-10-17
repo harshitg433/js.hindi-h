849ba387be6ea2a73a2fc84e820ba1a4baa20b2f
harshitg433 <119657161+harshitg433@users.noreply.github.com>
2023-10-17 15:43:54 +0000

    string

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -4,3 +4,32 @@ const repoCount = 50
 // console.log(name + repoCount + " Value");
 
 console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("harshit-g")
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('h'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-6,4)
+console.log(anotherString);
+
+const newStringOne = "  harshit   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+// console.log(gameName.replace('-','->'));
+
+console.log(gameName.includes('z'));
+
+// console.log(gameName.split('-'));
+const newStringTwo = newString.charAt(7);
+console.log(newStringTwo);
+console.log(newString.concat(',',newStringOne));
