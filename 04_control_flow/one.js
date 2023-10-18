8c0f18667eb2c4be73db4f03c5edac0b5213289a
harshitg433 <119657161+harshitg433@users.noreply.github.com>
2023-10-18 16:02:53 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,20 @@
+//if
+// const inUserLoggedIn = true
+
+// if (2 === "2") {
+//     console.log("executed");
+// } else {
+//     console.log("not executed");
+// }
+
+const balance = 1000
+
+// if(balance > 500) console.log("test"),console.log("test2");
+
+// if(balance < 500) {
+//     console.log("less than 500");
+// } else if(balance < 750) {
+//     console.log("less than 750");
+// } else {
+//     console.log("hbahbfk");
+// }
