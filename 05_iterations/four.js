2e48efa6ce9f290ac2c7a5942b3757eab6ac98b1
harshitg433 <119657161+harshitg433@users.noreply.github.com>
2023-10-21 11:50:06 +0000

    array iteration

---
+++ 05_iterations/four.js
@@ -0,0 +1,26 @@
+const myobject ={
+    js:'javascript',
+    cpp:'c++',
+    rb:'ruby',
+    swift:'swift by apple'
+}
+
+// for(const key in myobject) {
+//     console.log(myobject[key]);
+// }
+
+// const arr=[1,2,5,6,6,4,9]
+// for(const key in arr) {
+//     console.log(arr[key]);
+// }
+
+const map = new Map();
+map.set('IN',"India")
+map.set('USA',"United State of America")
+map.set('Fr',"France")
+map.set('UK',"United Kingndom")
+
+// for(const key in map) {
+//     console.log(map[key]);  // not iterable
+// }
+
