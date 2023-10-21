1ac478d8233f7f55b17547210f7d08ad9499521b
harshitg433 <119657161+harshitg433@users.noreply.github.com>
2023-10-21 10:31:56 +0000

    truthy

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,11 @@
+const userEmail ="abc@gmail.com"
+
+if(userEmail) {
+    console.log("got user email");
+} else {
+    console.log("don't have user email");
+}
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
