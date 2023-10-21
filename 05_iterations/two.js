1ce6f8febd947c50494be0ed9793a4088a88f08c
harshitg433 <119657161+harshitg433@users.noreply.github.com>
2023-10-21 11:25:43 +0000

    while and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,11 @@
+// let i=0
+// while (i < 5) {
+//     console.log(`${i}`);
+//     i = i + 2;
+// }
+
+let score = 1;
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score < 10);
