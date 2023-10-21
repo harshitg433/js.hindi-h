475c7ce73b1060a3c893592ce4aa731dad38544a
harshitg433 <119657161+harshitg433@users.noreply.github.com>
2023-10-21 11:43:24 +0000

    array

---
+++ 05_iterations/three.js
@@ -0,0 +1,35 @@
+// const arr =[1, 2, 3, 4, 5]
+
+// for (const num of arr) {
+//     console.log(num);
+
+// }
+
+// const gret= "hello world";
+
+// for(const s of gret) {
+//     console.log(s);
+// }
+
+//Map
+
+const map = new Map();
+map.set('IN',"India")
+map.set('USA',"United State of America")
+map.set('Fr',"France")
+map.set('UK',"United Kingndom")
+
+// console.log(map.get('IN'));
+
+// for(const [key] of map) {
+//     console.log([key]);
+// }
+
+const obj = {
+    'game1': 'NFS',
+    'game2': 'Spiderman'
+}
+
+// for(const [key, value] of obj) {
+//     console.log(key, ':-', value);
+// }
